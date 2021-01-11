83debcd9791cfcb9d7e8ee2301a905600f84bf24
Tom George <tg82490@gmail.com>
2021-01-11 17:39:46 -0600

    Reset pom
    Signed-off-by: Tom George <tg82490@gmail.com>

--- backend-base/src/main/java/org/eclipse/che/incubator/workspace/telemetry/generate/GenerateOpenApiSchema.java
+++ backend-base/src/main/java/org/eclipse/che/incubator/workspace/telemetry/generate/GenerateOpenApiSchema.java
@@ -3,16 +3,18 @@ package org.eclipse.che.incubator.workspace.telemetry.generate;
 import java.io.FileWriter;
 import java.io.IOException;
 
+import io.smallrye.config.SmallRyeConfig;
 import io.smallrye.config.SmallRyeConfigBuilder;
-import io.smallrye.openapi.runtime.io.Format;
 import org.eclipse.microprofile.config.Config;
+import org.eclipse.microprofile.config.ConfigProvider;
 import org.eclipse.microprofile.openapi.models.OpenAPI;
 import org.jboss.jandex.Index;
 import org.jboss.jandex.IndexReader;
 
 import io.smallrye.openapi.api.OpenApiConfigImpl;
-import io.smallrye.openapi.api.constants.OpenApiConstants;
+import io.smallrye.openapi.api.OpenApiConstants;
 import io.smallrye.openapi.runtime.io.OpenApiSerializer;
+import io.smallrye.openapi.runtime.io.OpenApiSerializer.Format;
 import io.smallrye.openapi.runtime.scanner.OpenApiAnnotationScanner;
 
 public class GenerateOpenApiSchema {
